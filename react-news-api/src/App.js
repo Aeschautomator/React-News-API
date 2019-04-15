632e40e6ebc7ae55bdd8587a944529ed83780f6c
Alex esch <eschmassage@gmail.com>
2019-04-15 12:23:16 -0400

    added search & result components

--- react-news-api/src/App.js
+++ react-news-api/src/App.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/Navbar/index.js';
-import SearchResults from './components/Results/Results';
-import SearchHistory from './components/History/History';
+import Results from './components/Results/Results';
+import History from './components/History/History';
 
 class App extends Component {
   render() {
-    return (<Navbar></Navbar>
+    
+    <Navbar />
+      <div className="container mt-5 text-center">
+       <div className="row">
+        <div className="col-6 border-right border-left border-dark">
+          <Results />
+          <div className="col-6 border-right border-dark">
+              <History />
+         </div>
+      
+      
+       </div>
+    </div>
  
- 
-   
-    );
+
   }
 }
 
